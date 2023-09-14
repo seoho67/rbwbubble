e7db98c4cddae2208563b1dbf440e651f214136a
seoho67 <144305589+seoho67@users.noreply.github.com>
2023-09-14 23:15:44 +0900

    Update getArtistCode.js

--- components/getArtistCode.js
+++ components/getArtistCode.js
@@ -4,9 +4,18 @@ const getArtistCode = (name) => {
         case '서  호':
             console.log('Artist: 이서호'); 
             return ('LSH'); 
+        case '이도':
+            console.log('Artist: 김건학'); 
+            return ('KKH');
         case '건희':
             console.log('Artist: 이건희'); 
-            return ('LKH');     
+            return ('LKH'); 
+        case '화눙':
+            console.log('Artist: 여환웅');
+            return ('YHW');
+        case '시온이':
+            console.log('Artist: 손동주');
+            return ('SDJ');
 
         default: return undefined;
     }
